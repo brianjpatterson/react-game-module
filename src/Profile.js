a60bb3cdae5c2d6913552d1cfc9bfa985abc59c5
Brian Patterson <cryptothick@gmail.com>
2019-11-29 14:49:05 -0800

    created escaped version

--- src/Profile.js
+++ src/Profile.js
@@ -1,13 +1,15 @@
 import React from "react";
+import escapeHtml from "escape-html";
 
 function Profile(props) {
+  console.log(escapeHtml);
   return (
     <div>
-      <h2>{props.name}</h2>
-      <img alt="user avatar" src={props.avatar} />
-      <a href={props.twitter}>Twitter</a>
-      <a href={props.facebook}>Facebook</a>
-      <a href={props.instagram}>Instagram</a>
+      <h2>{escapeHtml(props.name)}</h2>
+      <img alt="user avatar" src={escapeHtml(props.avatar)} />
+      <a href={escapeHtml(props.twitter)}>Twitter</a>
+      <a href={escapeHtml(props.facebook)}>Facebook</a>
+      <a href={escapeHtml(props.instagram)}>Instagram</a>
     </div>
   );
 }
